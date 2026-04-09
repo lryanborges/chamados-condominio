1b43a18fb0d09b206be89cb25c0c06bcb860e79a
Ryan Borges <ryanborgeslockser@gmail.com>
2026-04-09 16:25:48 -0300

    init: start project

---
+++ src/main/java/com/dunnas/chamados_condominio/ChamadosCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.dunnas.chamados_condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunnas/chamados_condominio/ChamadosCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunnas.chamados_condominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
