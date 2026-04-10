e50faf04be175bac675698a030cd85e434532e58
Ryan Borges <ryanborgeslockser@gmail.com>
2026-04-10 16:13:01 -0300

    feat: create units in blocks

---
+++ src/main/java/com/dunnas/chamados_condominio/infrastructure/persistence/UnitEntity.java
@@ -0,0 +1,54 @@
+package com.dunnas.chamados_condominio.infrastructure.persistence;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "unit")
+public class UnitEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long blockId;
+    private int floor;
+    private String identifier;
+
+    public UnitEntity() {}
+
+    public UnitEntity(Long blockId, int floor, String identifier) {
+        this.blockId = blockId;
+        this.floor = floor;
+        this.identifier = identifier;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBlockId() {
+        return blockId;
+    }
+
+    public void setBlockId(Long blockId) {
+        this.blockId = blockId;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+}
