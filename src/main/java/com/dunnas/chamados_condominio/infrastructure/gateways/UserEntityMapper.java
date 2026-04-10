da460bfe0434df1cb4fd1a932742407736aff01d
Ryan Borges <ryanborgeslockser@gmail.com>
2026-04-10 20:47:56 -0300

    feat: find-all-users

--- src/main/java/com/dunnas/chamados_condominio/infrastructure/gateways/UserEntityMapper.java
+++ src/main/java/com/dunnas/chamados_condominio/infrastructure/gateways/UserEntityMapper.java
@@ -3,6 +3,9 @@ package com.dunnas.chamados_condominio.infrastructure.gateways;
 import com.dunnas.chamados_condominio.domain.entity.User;
 import com.dunnas.chamados_condominio.infrastructure.persistence.UserEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserEntityMapper {
     UserEntity toEntity(User userDomainObj) {
         return new UserEntity(userDomainObj.getName(), userDomainObj.getEmail(), userDomainObj.getPassword(), userDomainObj.getRole(), userDomainObj.getScope());
@@ -10,5 +13,17 @@ public class UserEntityMapper {
 
     User toDomainObj(UserEntity userEntity) {
         return new User(userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getRole(), userEntity.getScope());
+    }
+
+    List<User> toDomainObjList(List<UserEntity> userEntityList) {
+        return userEntityList.stream()
+                .map(this::toDomainObj)
+                .toList();
+    }
+
+    List<UserEntity> toEntityList(List<User> userList) {
+        return userList.stream()
+                .map(this::toEntity)
+                .toList();
     }
 }
