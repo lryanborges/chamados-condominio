1d53c6ba18dfcea23858d8e0e77bb94fac83d2c0
Ryan Borges <ryanborgeslockser@gmail.com>
2026-04-15 22:01:28 -0300

    feat: find-annexes-by-call-id

--- src/main/java/com/dunnas/chamados_condominio/application/gateways/AnnexGateway.java
+++ src/main/java/com/dunnas/chamados_condominio/application/gateways/AnnexGateway.java
@@ -2,6 +2,9 @@ package com.dunnas.chamados_condominio.application.gateways;
 
 import com.dunnas.chamados_condominio.domain.entity.Annex;
 
+import java.util.List;
+
 public interface AnnexGateway {
     Annex createAnnex(Annex annex);
+    List<Annex> findAnnexesByCallId(Long callId);
 }
